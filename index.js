17da83e5a22171effb45a8161a5d3f272f18846d
kevdude749 <kevin.champagne8@gmail.com>
2016-02-18 20:47:22 -0600

    ???

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express')
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server, {"transports":["websocket"]}),
+	io = require('socket.io').listen(server, {"transports": ["xhr-polling"],"polling duration": 10}),
 
 app.set('port', (process.env.PORT || 5000));
 
