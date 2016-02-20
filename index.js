517172be7da49256a20c375dc86e846d18788a64
kevdude749 <kevin.champagne8@gmail.com>
2016-02-19 19:10:09 -0600

    asdfasDF

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-// Setup basic express server
 var WebSocketServer = require("ws").Server;
-
 var express = require('express')
 app = express(),
 server = require('http').createServer(app),
 io = require('socket.io').listen(server,{"pingInterval":4000,"pingTimeout":4000,"transports":["polling"]}),
-
 app.set('port', (process.env.PORT || 5000));
-
-
-
 server.listen(app.get('port'));
-
-// Routing
 app.use(express.static(__dirname + '/public'));
-
-// Chatroom
-
 var wss = new WebSocketServer({server: server});
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(message) {
+
+wss.on('connection', function(ws) {
+  ws.on('message', function(message) {
     console.log('received: %s', message);
   });
  
