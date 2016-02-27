d724dbf65458fe2de879f12298555188f67b30d7
kevdude749 <kevin.champagne8@gmail.com>
2016-02-27 08:15:20 -0600

    hi

--- public/js/game.js
+++ public/js/game.js
@@ -38,7 +38,7 @@ socket.bind("new tank",function(msg){
 		server_tanks[msg.username].arm = new_arm;
 		
 		x = msg.x;
-		y = msg.y + 35;
+		y = msg.y + 40;
 		server_tanks[msg.username].text = game.add.text(x,y,msg.username,{font:"20px Arial",fill:"#666666",align:'center'});
 		server_tanks[msg.username].text.anchor.set(0.5);
 	}
@@ -65,7 +65,7 @@ socket.bind("server tanks",function(msg){
 		server_tanks[server_tank].arm = new_arm;
 		
 		x = msg[server_tank].x;
-		y = msg[server_tank].y + 35;
+		y = msg[server_tank].y + 40;
 		server_tanks[server_tank].text = game.add.text(x,y,server_tank,{font:"20px Arial",fill:"#666666",align:'center'});
 		server_tanks[server_tank].text.anchor.set(0.5);
 		
@@ -80,9 +80,10 @@ socket.bind("server tanks",function(msg){
 
 socket.bind("update tank",function(msg){
 	if (msg.username != name){
-		server_tanks[msg.username].body.x = server_tanks[msg.username].arm.x = server_tanks[msg.username].text.x = msg.x;
-		server_tanks[msg.username].body.y = server_tanks[msg.username].arm.y = server_tanks[msg.username].text.y = msg.y;
-	
+		server_tanks[msg.username].body.x = server_tanks[msg.username].arm.x = msg.x;
+		server_tanks[msg.username].body.y = server_tanks[msg.username].arm.y = msg.y;
+		server_tanks[msg.username].text.x = msg.x + 40;
+		server_tanks[msg.username].text.y = msg.y + 40;
 		server_tanks[msg.username].arm.angle = msg.arm;
 	}
 });
